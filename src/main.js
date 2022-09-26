baedff88a3ce8217beceb55819334a0fc7c02e3c
Kate Kravchuk <k8jaim@gmail.com>
2022-09-26 21:45:04 +0300

    added: points api service, data adapter

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import FilterModel from './model/filter-model.js';
 
 import TripPresenter from './presenter/trip-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
+import PointsApiService from './points-api-service.js';
 
 const tripInfoContainerElement = document.querySelector('.trip-main');
 const filterContainerElement = tripInfoContainerElement.querySelector('.trip-controls__filters');
 const tripContainerElement = document.querySelector('.trip-events');
 const addPointButtonElement = tripInfoContainerElement.querySelector('.trip-main__event-add-btn');
 
-const pointsModel = new PointsModel();
+const AUTHORIZATION = 'Basic jv54vUVvkBk7tPTO';
+const END_POINT = 'https://18.ecmascript.pages.academy/big-trip';
+
+const pointsApiService = new PointsApiService(END_POINT, AUTHORIZATION);
+
+const pointsModel = new PointsModel(pointsApiService);
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
 const filterModel = new FilterModel();
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -39,8 +39,20 @@ const sortPointsByPriceDesc = (targetPoint, pointToCompare) => pointToCompare.ba
 
 export default class PointsModel extends Observable {
   #rawPoints = Array.from({length:5}, (_,index) => createPoint(index));
+  // #rawPoints = [];
   #filteredPoints = filter[FilterType.EVERYTHING](this.#rawPoints);
   #pointsDefaultSortOrder = this.#filteredPoints.sort(sortPointsByDateAsc);
+  #pointsApiService = null;
+
+  constructor (pointsApiService) {
+    super();
+    this.#pointsApiService = pointsApiService;
+
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+      console.log(points.map(this.#adaptToClient));
+    });
+  }
 
   get points() {
     return this.#pointsDefaultSortOrder;
@@ -104,5 +116,21 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType);
+  };
+
+  #adaptToClient = (point) => {
+    const adaptedPoint = {...point,
+      basePrice: point['base_price'],
+      dateFrom: new Date(point['date_from']),
+      dateTo: new Date(point['date_to']),
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
   };
 }
