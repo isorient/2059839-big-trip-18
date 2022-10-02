6e6518aabf78b9c938ce7fb236c8113418a072a2
Kate Kravchuk <k8jaim@gmail.com>
2022-10-02 17:50:10 +0300

    removed: draft notes

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -39,7 +39,6 @@ export default class TripPresenter {
   #currentSortType = SortType.DAY;
   #filterType = FilterType.EVERYTHING;
   #uiBlocker = new UiBlocker(TimeLimit.LOWER_LIMIT, TimeLimit.UPPER_LIMIT);
-  #tripInfo = null;
 
   #isLoading = true;
   #arePointsLoaded = false;
